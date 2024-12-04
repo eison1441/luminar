27d93faea3119c06c9ee1cafdd3fd1dbf1e8fed3
Eison <eisondanieljohnson007@gmail.com>
2024-12-04 23:10:30 +0530

    added python homework file

---
+++ homework/pythonhomework/task-9.py
@@ -0,0 +1,14 @@
+def mrg_interval(intervals):
+    intervals.sort(key=lambda x:x[0])
+
+    merged=[]
+
+    for interval in intervals:
+        if not merged or merged[-1][1]<interval[0]:
+            merged.append(interval)
+        else:
+            merged[-1][1]=max(merged[-1][1],interval[1])
+    return merged
+interval= [[1,3],[2,6],[8,10],[15,18]]
+merged_intervals=mrg_interval(interval)
+print(merged_intervals)
